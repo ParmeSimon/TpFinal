a2191e4566268a030e7fb654214f5898af1bf401
PARME Simon <simon.parme@renault.com>
2026-06-22 13:57:51 +0200

    create repository and security files

--- src/main/java/fr/ekod/cda/ja/tpfinal/repository/UserRepository.java
+++ src/main/java/fr/ekod/cda/ja/tpfinal/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package fr.ekod.cda.ja.tpfinal.repository;
 
-public interface UserRepository {
+import fr.ekod.cda.ja.tpfinal.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
